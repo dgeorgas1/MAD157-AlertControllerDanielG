9dfba1a0044001db2c58d31623754555aeaea629
Daniel Georgas <dgeorgas@students.mchenry.edu>
2024-02-25 23:20:04 -0600

    Commit changes

--- AlertController/ViewController.swift
+++ AlertController/ViewController.swift
@@ -11,9 +11,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Warning", message: "Zombies are loose!", preferredStyle: .alert)
+            //Styles
+                //Alert - Displays in the middle
+                //ActionSheet displays at the bottom
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in
+            //Dismisses the alert
+        })
+        
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 }
-
